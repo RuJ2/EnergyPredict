43b911360f34d2e2c32be13004eea48f2a7886f4
RuJ2 <49227852+RuJ2@users.noreply.github.com>
2021-10-14 18:52:45 +0800

    增加输入预测功能 caculation

--- RY_python2/server.py
+++ RY_python2/server.py
@@ -9,12 +9,10 @@ from flask import Flask, request
 import json
 import os
 import numpy as np
-from Regresser import Ensemble,ann_predict
+from ann import ann_predict
 
-ensemble = Ensemble()
-ensemble.load_model()
-    
 app = Flask(__name__)
+
 
 
 @app.route('/')
@@ -30,22 +28,26 @@ def predict():
     data = request.get_data()
     jdata = json.loads(data.decode("utf-8"))
     data_type = jdata['method'] 
-    # data_type = 'oven' # TEST
-    
+    # data_type = 'water' # TEST
+    print(data_type)
+	
     # 打开record数据
     with open(os.getcwd()+'/data/record.json','r') as load_f:
         datasets = json.load(load_f)
-	# print("datasets",datasets)
 	
+    
+    # 获取日期并排序 获得最近15天的数据
+    temp = []
+    for _i in datasets:
+        temp.append((_i,datasets[_i]["days"]))
+    temp.sort(key=lambda x:x[1],reverse=True)
+    
     predicts = {}
-    for id in datasets.keys():
-        # 对前15组数据进行预测
-        if int(id)<15:
-            featrues = np.array([[datasets[id]["speed"],datasets[id]["yield"]]])
-            predicts[id] = {'y_ann': ann_predict(featrues)[data_type],
-                            "y_ensemble": ensemble.predict(featrues)[data_type],
-							"date": datasets[id]['days']}
-                       
+    for i in range(15):
+        id = temp[i][0]
+        featrues = np.array([[datasets[id]["speed"]]])
+        predicts[id] = {"y":datasets[id][data_type],"y_ann":ann_predict(featrues)[data_type],"date": temp[i][1]}
+        
     # 打印预测结果
     print("预测数据返回···\n",predicts)
     
@@ -66,6 +68,22 @@ def record():
     print('record success!')
     
     return ''
+
+@app.route('/caculation',methods=['POST'])
+def caculation():
+    print("运行caculation···")
+    data = request.get_data()
+    jdata = json.loads(data.decode("utf-8"))
+    predicts = {}
+    
+    featrues = np.array([[jdata["speed"]]])
+    predicts = ann_predict(featrues)
+    
+    return_data = dict(jdata,**predicts)
+    print(return_data)
+    return return_data
+
+	
 
 if __name__ == '__main__':
     print("运行main···")
